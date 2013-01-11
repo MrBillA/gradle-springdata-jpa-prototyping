abe5193ba20009a8c57c07f20a9f0c6fb40066d9
Bill Andrews <ww.andrews@comcast.net>
2013-01-10 17:25:28 -0800

    Cleanup of configuration settings.   gradle -Penv=dev|test|prod     see config.groovy

--- config.groovy
+++ config.groovy
@@ -6,18 +6,19 @@ mail {
 environments {
     dev {
         serverName = 'http://localhost:9090'
-        }
-            test {
-                serverName = 'http://testserver'
-                mail {
-                    server = 'mail.testserver'
-                    }
-                }
-            prod {
-                serverName = 'http://www.mrhaki.com'
-                mail {
-                    port = 552
-                    server = 'mail.host.com'
-                    }
-                }
     }
+
+	test {
+		serverName = 'http://testserver'
+		mail {
+			server = 'mail.testserver'
+			}
+	}
+	prod {
+		serverName = 'http://www.mrhaki.com'
+		mail {
+			port = 552
+			server = 'mail.host.com'
+		}
+	}
+}
